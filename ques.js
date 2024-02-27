91de4af2b9eb955deefcdd1cc089c5ef3698cd52
NISHANT <thakurnishant0420@gmail.com>
2024-02-27 18:35:52 +0530

    array-object js ques

--- ques.js
+++ ques.js
@@ -16,3 +16,25 @@ function avgscore(students){
     return c/students.length
 }
 console.log(avgscore(students))
+
+const library = [
+    { title: 'The Great Gatsby', author: 'F. Scott Fitzgerald', pages: 180 },
+    { title: 'To Kill a Mockingbird', author: 'Harper Lee', pages: 281 },
+    { title: '1984', author: 'George Orwell', pages: 328 },
+    { title: 'Pride and Prejudice', author: 'Jane Austen', pages: 432 },
+    { title: 'Moby Dick', author: 'Herman Melville', pages: 720 }
+  ];
+
+//   function to return book title with max pages.
+function longestBook(library) {
+    const pagesArray = library.map(book => book.pages); //map and filter can be used like this
+    // map basically takes any particular value from object or array and transfer to other
+    const maxPages = Math.max(...pagesArray);
+    const longestBook = library.find(book => book.pages === maxPages);
+    return longestBook.title;
+}
+console.log(longestBook(library));
+
+// in above code we first take all pages value to other array with map and than take the max value from that 
+// array with this max value i filter that object with max pages and then from that i return book title.
+
