3648db6345586d44dc4ffb0e2d21972c7c25a9bb
NISHANT <thakurnishant0420@gmail.com>
2024-02-27 18:34:55 +0530

    array and object ques

---
+++ ques.js
@@ -0,0 +1,18 @@
+const students = [
+    { name: 'Alice', score: 85 },
+    { name: 'Bob', score: 90 },
+    { name: 'Charlie', score: 75 },
+    { name: 'David', score: 95 },
+    { name: 'Eva', score: 88 }
+  ];
+  
+// average score
+function avgscore(students){
+    let c=0
+    students.forEach(n => {
+        c+=n.score
+        
+    });
+    return c/students.length
+}
+console.log(avgscore(students))
